8ccfa94c63ab35c3b9bfb9d43a6a2ffd556f68ce
Bavi <bavidhraa.shrrei@kanini.com>
2021-01-27 23:19:27 +0530

    Add project files.

---
+++ Fnd_emply_nme/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fnd_emply_nme
+{
+    public class employeedetails
+    {
+        List<string> emp_name = new List<string>();
+        string name;
+        public void Add_employee_name()
+        {
+            do
+            {
+                name = Console.ReadLine();
+                emp_name.Add(name);
+            } while (!string.IsNullOrEmpty(name));
+        }
+        public void Find_Poistion()
+        {
+            int index = 0;
+            name = Console.ReadLine();
+            index= emp_name.FindIndex(a => a.Contains(name));
+            Console.WriteLine("{0} is the position {1} for promotion",name,index+1);
+
+          }
+    }
+}
---
+++ Fnd_emply_nme/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fnd_emply_nme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employeedetails em = new employeedetails();
+            Console.WriteLine("Please enter the employee names in the order of their eligibility for promotion");
+            em.Add_employee_name();
+            Console.WriteLine("Please enter the name of the employee to check promotion position");
+            em.Find_Poistion();
+            Console.ReadKey();
+            }
+    }
+}
